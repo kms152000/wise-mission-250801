25e03d00bbd400cb4f6a5e076dc42a00cc4489a1
kms152000 <kms152000@naver.com>
2025-08-03 22:21:20 +0900

    2단계 완료

---
+++ com/back/Main.java
@@ -0,0 +1,27 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                sc.nextLine();
+                System.out.print("작가 : ");
+                sc.nextLine();
+            }
+            else if (command.equals("종료")) {
+                System.out.println("앱을 종료합니다.");
+                break;
+            }
+        }
+    }
+}
