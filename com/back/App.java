9c2a37cedfb2934db6aa159106c5a2d4597e3ea2
kms152000 <kms152000@naver.com>
2025-08-05 23:35:31 +0900

    8단계 완료

--- com/back/App.java
+++ com/back/App.java
@@ -26,6 +26,9 @@ public class App {
             else if (command.startsWith("삭제")) {
                 actionDelete(command);
             }
+            else if (command.startsWith("수정")) {
+                actionModify(command);
+            }
             else if (command.equals("종료")) {
                 break;
             }
@@ -98,15 +101,17 @@ public class App {
         }
     }
 
-    public boolean delete(int id) {
-        int deleteTargetIndex = -1; // 삭제하고 싶은 명언이 저장된 위치
-
+    public int findIndexById(int id) {
         for (int i = 0; i < lastIndex; i++) {
             if (wiseSayings[i].id == id) {
-                deleteTargetIndex = i;
-                break;
+                return i;
             }
         }
+        return -1;
+    }
+
+    public boolean delete(int id) {
+        int deleteTargetIndex = findIndexById(id);
 
         if (deleteTargetIndex == -1) {
             return false; // 삭제할 명언이 존재하지 않음
@@ -118,5 +123,41 @@ public class App {
         lastIndex--;
 
         return true; // 삭제 성공
+    }
+
+    public void actionModify(String command) {
+
+        String[] commandBits = command.split("=");
+
+        if (commandBits.length < 2) {
+            System.out.println("번호를 입력해주세요.");
+            return;
+        }
+
+        String idStr = commandBits[1];
+        int id = Integer.parseInt(idStr);
+
+        int modifyTargetIndex = findIndexById(id);
+
+        if(modifyTargetIndex == -1) {
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
+
+        WiseSaying modifyTargetWiseSaying = wiseSayings[modifyTargetIndex];
+
+        System.out.println("명언(기존) : %s".formatted(modifyTargetWiseSaying.saying));
+        System.out.print("명언 : ");
+        String newSaying = sc.nextLine();
+        System.out.println("작가(기존) : %s".formatted(modifyTargetWiseSaying.author));
+        System.out.print("작가 : ");
+        String newAuthor = sc.nextLine();
+
+        modify(modifyTargetWiseSaying, newSaying, newAuthor);
+    }
+
+    public void modify(WiseSaying modifyTargetWiseSaying, String newSaying, String newAuthor) {
+        modifyTargetWiseSaying.saying = newSaying;
+        modifyTargetWiseSaying.author = newAuthor;
     }
 }
